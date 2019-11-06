b366ae3ccb06e93f7e65518ce3ac3ae1b641d8db
Miao <email@example.com>
2019-11-06 13:11:55 +0800

    实现退出登陆功能

--- src/main/java/com/Miao/community/Controller/AuthorizeController.java
+++ src/main/java/com/Miao/community/Controller/AuthorizeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
 import java.util.UUID;
@@ -33,6 +34,7 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String uri;
 
+    //登陆
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state,
@@ -64,5 +66,16 @@ public class AuthorizeController {
             //登陆失败，重新登陆
             return "redirect:/";
         }
+    }
+
+    //退出登陆
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request,
+                         HttpServletResponse response) {
+        request.getSession().removeAttribute("user");
+        Cookie cookie = new Cookie("token", null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return "redirect:/";
     }
 }
